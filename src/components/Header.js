aa99c2110c590200dc54838a6e41bf304923a255
RamySaleh <ramy.saleh.me@gmail.com>
2020-06-06 15:56:57 +0100

    header css

--- src/components/Header.js
+++ src/components/Header.js
@@ -17,93 +17,98 @@ const Header = (props) => {
   };
 
   return (
-    <div style={{ margin: 10 }}>
-      <button
-        onClick={() =>
-          handleNewGameButtonClick({
-            type: Constants.GamePresetRegular,
-            level: 1,
-          })
-        }
-        style={{ marginRight: 10 }}
-      >
-        Beginner
-      </button>
-      <button
-        onClick={() =>
-          handleNewGameButtonClick({
-            type: Constants.GamePresetRegular,
-            level: 2,
-          })
-        }
-        style={{ marginRight: 10 }}
-      >
-        Intermediate
-      </button>
-      <button
-        onClick={() =>
-          handleNewGameButtonClick({
-            type: Constants.GamePresetRegular,
-            level: 3,
-          })
-        }
-        style={{ marginRight: 10 }}
-      >
-        Expert
-      </button>
-      <button
-        onClick={() => {
-          setIsCustom(true);
-        }}
-      >
-        Custom
-      </button>
+    <div
+      style={{
+        margin: 10,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <div>
+        <button
+          onClick={() =>
+            handleNewGameButtonClick({
+              type: Constants.GamePresetRegular,
+              level: 1,
+            })
+          }
+          style={{ marginRight: 10 }}
+        >
+          Beginner
+        </button>
+        <button
+          onClick={() =>
+            handleNewGameButtonClick({
+              type: Constants.GamePresetRegular,
+              level: 2,
+            })
+          }
+          style={{ marginRight: 10 }}
+        >
+          Intermediate
+        </button>
+        <button
+          onClick={() =>
+            handleNewGameButtonClick({
+              type: Constants.GamePresetRegular,
+              level: 3,
+            })
+          }
+          style={{ marginRight: 10 }}
+        >
+          Expert
+        </button>
+        <button
+          onClick={() => {
+            setIsCustom(true);
+          }}
+        >
+          Custom
+        </button>
+      </div>
       {isCustom && (
-        <div style={{ flexDirection: "row", flex: 1 }}>
-          <div>
-            <text>w</text>
-            <input
-              style={{ width: 40 }}
-              text={width}
-              onChange={({ target }) =>
-                handleTextChange(target.value, setWidth)
-              }
-            ></input>
-          </div>
-          <div>
-            <text>h</text>
-            <input
-              style={{ width: 40 }}
-              text={height}
-              onChange={({ target }) =>
-                handleTextChange(target.value, setheight)
-              }
-            ></input>
-          </div>
-          <div>
-            <text>b</text>
-            <input
-              style={{ width: 40 }}
-              text={bombs}
-              onChange={({ target }) =>
-                handleTextChange(target.value, setBombs)
-              }
-            ></input>
-          </div>
-          <div>
-            <button
-              onClick={() =>
-                handleNewGameButtonClick({
-                  type: Constants.GamePresetCustom,
-                  width,
-                  height,
-                  bombs,
-                })
-              }
-            >
-              Start
-            </button>
-          </div>
+        <div
+          style={{
+            justifyContent: "space-between",
+            display: "flex",
+            width: 300,
+            margin: 10,
+          }}
+        >
+          <text>w</text>
+          <input
+            style={{ width: 40 }}
+            text={width}
+            onChange={({ target }) => handleTextChange(target.value, setWidth)}
+          ></input>
+
+          <text>h</text>
+          <input
+            style={{ width: 40 }}
+            text={height}
+            onChange={({ target }) => handleTextChange(target.value, setheight)}
+          ></input>
+
+          <text>b</text>
+          <input
+            style={{ width: 40 }}
+            text={bombs}
+            onChange={({ target }) => handleTextChange(target.value, setBombs)}
+          ></input>
+
+          <button
+            onClick={() =>
+              handleNewGameButtonClick({
+                type: Constants.GamePresetCustom,
+                width,
+                height,
+                bombs,
+              })
+            }
+          >
+            Start
+          </button>
         </div>
       )}
     </div>
