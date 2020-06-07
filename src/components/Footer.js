0d0ff0a2110de0e843f8a8d20a41c6b7cd147e9f
RamySaleh <ramy.saleh.me@gmail.com>
2020-06-07 15:27:21 +0100

    toast message during 5s

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -87,7 +87,7 @@ const Footer = () => {
       {showTimer() && (
         <text style={{ marginLeft: 20 }}>{formatTime(timer)}</text>
       )}
-      <Snackbar open={open} autoHideDuration={3000} onClose={handleClose}>
+      <Snackbar open={open} autoHideDuration={5000} onClose={handleClose}>
         <Alert
           onClose={handleClose}
           severity={isGameWon() ? "success" : "error"}
